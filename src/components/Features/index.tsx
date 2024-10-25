b00f20e9c1056547b3933e70a47d42744942b6e7
AbrahamAlemu-github <abraham1123alemu@gmail.com>
2024-10-25 19:12:27 -0400

    Implementing a Main Features component

--- src/components/Features/index.tsx
+++ src/components/Features/index.tsx
@@ -1,26 +1,26 @@
+// src/components/Features/Features.tsx
+
 import SectionTitle from "../Common/SectionTitle";
 import SingleFeature from "./SingleFeature";
 import featuresData from "./featuresData";
 
 const Features = () => {
   return (
-    <>
-      <section id="features" className="py-16 md:py-20 lg:py-28">
-        <div className="container">
-          <SectionTitle
-            title="Main Features"
-            paragraph="There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form."
-            center
-          />
+    <section id="features" className="py-16 md:py-20 lg:py-28 bg-white dark:bg-gray-800">
+      <div className="container mx-auto px-4">
+        <SectionTitle
+          title="Main Features"
+          paragraph="3Spans offers a robust multicalculus modeling environment, enabling mathematicians and students to visualize and solve complex problems with precision."
+          center
+        />
 
-          <div className="grid grid-cols-1 gap-x-8 gap-y-14 md:grid-cols-2 lg:grid-cols-3">
-            {featuresData.map((feature) => (
-              <SingleFeature key={feature.id} feature={feature} />
-            ))}
-          </div>
+        <div className="grid grid-cols-1 gap-x-8 gap-y-14 md:grid-cols-2 lg:grid-cols-3">
+          {featuresData.map((feature) => (
+            <SingleFeature key={feature.id} feature={feature} />
+          ))}
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
